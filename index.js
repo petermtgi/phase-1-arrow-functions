d88aad089dfa258b682d9e80a0ea2afeb930251f
Peter Mutungi <3sjk0krrr@gmail.com>
2025-03-21 09:44:29 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression for divide
+const divide = function () {
+    return 2000 / 100;
+  };
+  
+  // Arrow function for square
+  const square = x => x * x;
+  
+  // Arrow function for add
+  const add = (a, b) => a + b;
+  
+  // Export functions for testing
+  module.exports = { divide, square, add };
+  
